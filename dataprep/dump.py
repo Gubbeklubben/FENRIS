07214ea5de33d857704feec7287f012eddcdd20e
aopedal <alexander@opedal.org>
2026-01-28 10:29:46 +0100

    Get correct lung cancer dataset from Kagglehub

--- dataprep/dump.py
+++ dataprep/dump.py
@@ -1,10 +1,20 @@
-from datasets import Dataset
+from pathlib import Path
+from datasets import Dataset, load_dataset
 from sklearn.datasets import fetch_openml, load_breast_cancer
+import kagglehub
 
-def to_huggingface_dataset(data, target):
+def openml_to_huggingface_dataset(data, target):
     return Dataset.from_pandas(
         data.assign(label=target)
     )
+
+def kaggle_to_huggingface_dataset(dataset_name):
+    path = kagglehub.dataset_download(dataset_name)
+    csv_files = [str(p) for p in Path(path).iterdir() if p.suffix.lower() == ".csv"]
+    if not csv_files:
+        raise FileNotFoundError(f"No CSV files found in {path}")
+
+    return Dataset.from_csv(csv_files)
 
 def load_uci_heart_disease_dataset():
     dataset = fetch_openml(
@@ -12,21 +22,19 @@ def load_uci_heart_disease_dataset():
         version=1,
         as_frame=True,
     )
-    return to_huggingface_dataset(dataset.data, dataset.target)
+    return openml_to_huggingface_dataset(dataset.data, dataset.target)
 
 def load_breast_cancer_wisconsin_dataset():
     dataset = load_breast_cancer(
         as_frame=True,
     )
-    return to_huggingface_dataset(dataset.data, dataset.target)
+    return openml_to_huggingface_dataset(dataset.data, dataset.target)
 
 def load_ncctg_lung_cancer_dataset():
-    dataset = fetch_openml(
-        name="lung-cancer",
-        version=1,
-        as_frame=True,
+    return (
+        kaggle_to_huggingface_dataset("ukveteran/ncctg-lung-cancer-data")
+        .remove_columns(["Unnamed: 0"])
     )
-    return to_huggingface_dataset(dataset.data, dataset.target)
 
 heart_disease = load_uci_heart_disease_dataset()
 breast_cancer = load_breast_cancer_wisconsin_dataset()
