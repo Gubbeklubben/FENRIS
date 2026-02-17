a154c3e0f6f035979ab28210c849bfbf5deb1d1d
aopedal <alexander@opedal.org>
2026-02-17 16:45:34 +0100

    Nuke useless tests

--- tests/eval/test_context.py
+++
@@ -1,5 +0,0 @@
-def test_eval_context_fields(eval_ctx):
-    assert eval_ctx.train_df is not None
-    assert eval_ctx.synthetic_df is not None
-    assert eval_ctx.target_column == "label"
-    assert "cat" in eval_ctx.sensitive_columns
