ea11c36ad36f3a3685578f0a2c9b255cd1263a7a
bardlinga <bardlinga@gmail.com>
2026-03-17 17:03:08 +0100

    remove temp test script

--- src/fedbench/algorithms/fed_tgan/tests.py
+++
@@ -1,9 +0,0 @@
-from fedbench.algorithms.fed_tgan.fed_tgan import FedTGAN
-
-algo = FedTGAN()
-aggregator = algo.create_aggregator()
-synthesizer = algo.create_synthesizer()
-
-print(type(algo).__name__)
-print(type(aggregator).__name__)
-print(type(synthesizer).__name__)
