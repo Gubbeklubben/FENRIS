08c66e572a63f5b6ae7300f709d0949eb6ddd435
Loenning <Loenning1995@gmail.com>
2026-03-17 12:03:40 +0100

    Ran ruff

--- src/fedbench/partitioners/flwr_delegates.py
+++ src/fedbench/partitioners/flwr_delegates.py
@@ -18,7 +18,6 @@ from fedbench.core.data.partitioner import Partitioner
 
 
 class FlwrDelegatePartitioner(Partitioner):
-
     @classmethod
     def with_iid_partitioner(cls, num_partitions: int) -> Partitioner:
         return cls(IidPartitioner(num_partitions))
@@ -37,82 +36,91 @@ class FlwrDelegatePartitioner(Partitioner):
 
     @classmethod
     def with_dirichlet_partitioner(
-            cls,
-            num_partitions: int,
-            partition_by: str,
-            alpha: float,
-            min_partition_size: int = 10,
-            self_balancing: bool = False,
-            shuffle: bool = True,
-            seed: int | None = 42,
+        cls,
+        num_partitions: int,
+        partition_by: str,
+        alpha: float,
+        min_partition_size: int = 10,
+        self_balancing: bool = False,
+        shuffle: bool = True,
+        seed: int | None = 42,
     ) -> Partitioner:
-        return cls(DirichletPartitioner(
-            num_partitions,
-            partition_by=partition_by,
-            alpha=alpha,
-            min_partition_size=min_partition_size,
-            self_balancing=self_balancing,
-            shuffle=shuffle,
-            seed=seed,
-        ))
+        return cls(
+            DirichletPartitioner(
+                num_partitions,
+                partition_by=partition_by,
+                alpha=alpha,
+                min_partition_size=min_partition_size,
+                self_balancing=self_balancing,
+                shuffle=shuffle,
+                seed=seed,
+            )
+        )
 
     @classmethod
     def with_pathological_partitioner(
-            cls,
-            num_partitions: int,
-            partition_by: str,
-            num_classes_per_partition: int,
-            class_assignment_mode: Literal["random", "deterministic", "first-deterministic"] = "random",
-            shuffle: bool = True,
-            seed: int | None = 42,
+        cls,
+        num_partitions: int,
+        partition_by: str,
+        num_classes_per_partition: int,
+        class_assignment_mode: Literal[
+            "random", "deterministic", "first-deterministic"
+        ] = "random",
+        shuffle: bool = True,
+        seed: int | None = 42,
     ) -> Partitioner:
-        return cls(PathologicalPartitioner(
-            num_partitions,
-            partition_by=partition_by,
-            num_classes_per_partition=num_classes_per_partition,
-            class_assignment_mode=class_assignment_mode,
-            shuffle=shuffle,
-            seed=seed,
-        ))
+        return cls(
+            PathologicalPartitioner(
+                num_partitions,
+                partition_by=partition_by,
+                num_classes_per_partition=num_classes_per_partition,
+                class_assignment_mode=class_assignment_mode,
+                shuffle=shuffle,
+                seed=seed,
+            )
+        )
 
     @classmethod
     def with_shard_partitioner(
-            cls,
-            num_partitions: int,
-            partition_by: str,
-            num_shards_per_partition: int | None = None,
-            shard_size: int | None = None,
-            keep_incomplete_shard: bool = False,
-            shuffle: bool = True,
-            seed: int | None = 42,
+        cls,
+        num_partitions: int,
+        partition_by: str,
+        num_shards_per_partition: int | None = None,
+        shard_size: int | None = None,
+        keep_incomplete_shard: bool = False,
+        shuffle: bool = True,
+        seed: int | None = 42,
     ) -> Partitioner:
-        return cls(ShardPartitioner(
-            num_partitions,
-            partition_by=partition_by,
-            num_shards_per_partition=num_shards_per_partition,
-            shard_size=shard_size,
-            keep_incomplete_shard=keep_incomplete_shard,
-            shuffle=shuffle,
-            seed=seed,
-        ))
+        return cls(
+            ShardPartitioner(
+                num_partitions,
+                partition_by=partition_by,
+                num_shards_per_partition=num_shards_per_partition,
+                shard_size=shard_size,
+                keep_incomplete_shard=keep_incomplete_shard,
+                shuffle=shuffle,
+                seed=seed,
+            )
+        )
 
     @classmethod
     def with_continuous_partitioner(
-            cls,
-            num_partitions: int,
-            partition_by: str,
-            strictness: float,
-            shuffle: bool = True,
-            seed: int | None = 42,
+        cls,
+        num_partitions: int,
+        partition_by: str,
+        strictness: float,
+        shuffle: bool = True,
+        seed: int | None = 42,
     ) -> Partitioner:
-        return cls(ContinuousPartitioner(
-            num_partitions,
-            partition_by=partition_by,
-            strictness=strictness,
-            shuffle=shuffle,
-            seed=seed,
-        ))
-
+        return cls(
+            ContinuousPartitioner(
+                num_partitions,
+                partition_by=partition_by,
+                strictness=strictness,
+                shuffle=shuffle,
+                seed=seed,
+            )
+        )
 
     def __init__(self, flwr_partitioner: FlwrPartitioner):
         self._flwr_partitioner = flwr_partitioner
