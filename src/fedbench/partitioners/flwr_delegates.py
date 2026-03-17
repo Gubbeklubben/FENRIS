486eaf6484ad0941436ee5d35ceca9e02543e39c
Loenning <Loenning1995@gmail.com>
2026-03-17 12:52:50 +0100

    Revert changes made to load_partition

--- src/fedbench/partitioners/flwr_delegates.py
+++ src/fedbench/partitioners/flwr_delegates.py
@@ -144,9 +144,11 @@ class FlwrDelegatePartitioner(Partitioner):
         seed: int,
         test_size: float,
     ) -> DataFrame:
+
+        # noinspection PyUnnecessaryCast
         return cast(
             DataFrame,
             self._flwr_partitioner.load_partition(partition_id)
             .train_test_split(test_size=test_size, seed=seed)[split]
-            .with_format("pandas")[:],
+            .to_pandas(),  # makes a copy; mutations will not affect underlying dataset
         )
