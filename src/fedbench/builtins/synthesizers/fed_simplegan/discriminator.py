cf40f7898468c05eddd9610f067c7191a7ccfc71
bardlinga <bardlinga@gmail.com>
2026-04-06 13:17:07 +0200

    Copy contents to fed_simplegan package

---
+++ src/fedbench/builtins/synthesizers/fed_simplegan/discriminator.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
+
+
+class Discriminator(nn.Module):  # type: ignore[misc]
+    def __init__(self, input_dim: int):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)  # Output probability
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return torch.sigmoid(self.fc3(x))  # Probability of being real
