9271bd738bbd9bfaccdd2e567bb65265f2ebd251
trondthorsteinsson <trondthorsteinsson@gmail.com>
2026-04-09 16:39:45 +0200

    Project scaffolding wip.
    Add tomlkit dependency. Choose tomlkit for the ability
    to update an existing pyproject.toml without loosing
    comments or restructuring the file.
    
    Give new command the ability to generate a basic toml, as
    a start.

--- src/fedbench/cli/main.py
+++ src/fedbench/cli/main.py
@@ -1,11 +1,13 @@
 import typer
 
+from fedbench.cli.new import app as new
 from fedbench.cli.run import app as run
 from fedbench.cli.show import app as show
 
 app = typer.Typer()
 app.add_typer(show)
 app.add_typer(run)
+app.add_typer(new)
 
 
 if __name__ == "__main__":
