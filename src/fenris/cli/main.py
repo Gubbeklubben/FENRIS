b8ef6e8836a3e19ae6db2b4663f09184eeb00661
aopedal <alexander@opedal.org>
2026-05-10 15:30:32 +0200

    Move some imports to function/method level to increase startup speed

--- src/fenris/cli/main.py
+++ src/fenris/cli/main.py
@@ -5,7 +5,15 @@ from fenris.cli.plugin import app as plugins
 from fenris.cli.run import app as run
 from fenris.cli.show import app as show
 
-app = typer.Typer()
+app = typer.Typer(
+    context_settings={
+        "help_option_names": [
+            "-h",
+            "--help",
+            "--fenris-help",  # Alias for profiling. PyCharm's profiler eats --help
+        ]
+    }
+)
 app.add_typer(show)
 app.add_typer(run)
 app.add_typer(plugins, name="plugin")
