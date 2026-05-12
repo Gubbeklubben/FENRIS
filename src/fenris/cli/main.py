6ad26c4a52befad18058f69406d01f86e7533c96
aopedal <alexander@opedal.org>
2026-05-12 11:55:59 +0200

    Add --version option.

--- src/fenris/cli/main.py
+++ src/fenris/cli/main.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 import typer
 
 from fenris.cli.generate_schema import app as generate_schema
@@ -18,6 +20,26 @@ app.add_typer(show)
 app.add_typer(run)
 app.add_typer(plugins, name="plugin")
 app.add_typer(generate_schema)
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(version("fenris"))
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    _version: bool = typer.Option(
+        False,
+        "--version",
+        "-v",
+        callback=_version_callback,
+        is_eager=True,
+        help="Show version and exit.",
+    ),
+) -> None:
+    pass
 
 
 if __name__ == "__main__":
